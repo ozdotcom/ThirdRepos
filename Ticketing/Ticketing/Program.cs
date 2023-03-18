2ceafa28048030e3135d18c2db73d02e6c6430c8
000592349 <sharryn.hebberman@student.tafesa.edu.au>
2023-03-18 11:18:05 +1030

    update Ticketing form

--- Ticketing/Ticketing/Program.cs
+++ Ticketing/Ticketing/Program.cs
@@ -1,9 +1,9 @@
-
-using System;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
-namespace MyNameSpace
+namespace Ticketing
 {
     static class Program
     {
